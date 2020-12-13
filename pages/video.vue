1a7f35bb75c7edaa570a8424323f9909a87d1d08
Rakib Hossain <rakibhoossain@gmail.com>
2020-12-13 23:23:46 +0600

    grid

--- pages/video.vue
+++ pages/video.vue
@@ -1,34 +1,19 @@
 <template>
 <div>
-  <nuxt-link to="/inspire">Inspire</nuxt-link>
-  <vue-plyr>
-    <div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
-  </vue-plyr>
-  <vue-plyr>
-  <video
-    controls
-    crossorigin
-    playsinline
-    data-poster="https://i.ytimg.com/vi_webp/D63cFlh7TiI/maxresdefault.webp"
-  >
-    <source
-      size="720"
-      src="file:///C:/Users/serak/Downloads/screen-capture%20(20).webm"
-      type="video/mp4"
-    />
-    <source
-      size="1080"
-      src="file:///C:/Users/serak/Downloads/screen-capture%20(20).webm"
-      type="video/mp4"
-    />
-    <track
-      default
-      kind="captions"
-      label="English captions"
-      src="/path/to/english.vtt"
-      srclang="en"
-    />
-  </video>
-</vue-plyr>
+  <!-- <nuxt-link to="/inspire">Inspire</nuxt-link> -->
+  <v-container
+      class="mb-6"
+    >
+      <v-row>
+        <v-col
+          v-for="n in 10"
+          :key="n"
+        >
+          <vue-plyr>
+            <div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+          </vue-plyr>
+        </v-col>
+      </v-row>
+    </v-container>
 </div>
 </template>
