117978d282a1a11d1f1c28f7571024cd2b9c0ad0
Rakib Hossain <rakibhoossain@gmail.com>
2020-12-15 22:55:39 +0600

    vue plugin

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+   <h1>Hello</h1>
+</template>
+<script>
+export default {
+    name: "Test",
+    mounted() {
+        axios
+        .get(
+          `http://127.0.0.1:8000/api/shop?` +
+            new URLSearchParams(this.filter).toString()
+        ).then(res=>{
+            console.log(res)
+        }).catch(err=>{
+
+        })
+    },
+}
+</script>
