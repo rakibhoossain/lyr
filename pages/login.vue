faa232d6c0690aa9a0eacc9bd7761ca90f06c877
Rakib Hossain <rakibhoossain@gmail.com>
2020-12-22 00:17:52 +0600

    login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,59 +1,71 @@
 <template>
   <div>
-    <section class="hero is-info">
-      <div class="hero-body">
-        <div class="container is-desktop">
-          <div class="columns">
-            <div class="column">
-              <h1 class="title">
-                Log in
-              </h1>
-              <h2 class="subtitle">
-                Log in to your account!
-              </h2>
+    <v-app>
+        <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
+            <div>
+                <v-tabs v-model="tab" show-arrows background-color="deep-purple accent-4" icons-and-text dark grow>
+                    <v-tabs-slider color="purple darken-4"></v-tabs-slider>
+                    <v-tab v-for="(item, i) in tabs" :key="i">
+                        <v-icon large>{{ item.icon }}</v-icon>
+                        <div class="caption py-1">{{ item.name }}</div>
+                    </v-tab>
+                    <v-tab-item>
+                        <v-card class="px-4">
+                            <v-card-text>
+                                <v-form ref="loginForm" v-model="valid" lazy-validation>
+                                    <v-row>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="login.email" :class="errors && errors.email ? 'is-danger' : ''" :rules="loginEmailRules" label="E-mail" required></v-text-field>
+                                            <p class="help is-danger" v-if="errors && errors.email">{{ errors.email[0] }}</p>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="login.password" :append-icon="show1?'eye':'eye-off'" :class="errors && errors.password ? 'is-danger' : ''" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                                            <p class="help is-danger" v-if="errors && errors.password">{{ errors.password[0] }}</p>
+                                        </v-col>
+                                        <v-col class="d-flex" cols="12" sm="6" xsm="12">
+                                        </v-col>
+                                        <v-spacer></v-spacer>
+                                        <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                                            <v-btn x-large block :disabled="!valid" color="success" @click="userLogin"> Login </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-form>
+                            </v-card-text>
+                        </v-card>
+                    </v-tab-item>
+                    <v-tab-item>
+                        <v-card class="px-4">
+                            <v-card-text>
+                                <v-form ref="registerForm" v-model="valid" lazy-validation>
+                                    <v-row>
+                                        <v-col cols="12" sm="6" md="6">
+                                            <v-text-field v-model="firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="6">
+                                            <v-text-field v-model="lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
+                                        </v-col>
+                                        <v-spacer></v-spacer>
+                                        <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                                            <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-form>
+                            </v-card-text>
+                        </v-card>
+                    </v-tab-item>
+                </v-tabs>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="section content">
-      <div class="container">
-        <div class="columns is-centered">
-          <div class="column is-4">
-            <form @submit.prevent="userLogin">
-
-                <div v-if="errors && errors.message" class="notification is-danger">
-                {{ errors.message }}
-                </div>
-
-                <div class="field">
-                <div class="control">
-                    <label class="label">Email</label>
-                    <input type="email" placeholder="Your Email" class="input" :class="errors && errors.email ? 'is-danger' : ''" v-model="login.email" />
-                    <p class="help is-danger" v-if="errors && errors.email">{{ errors.email[0] }}</p>
-                </div>
-                </div>
-
-                <div class="field">
-                <div class="control">
-                    <label class="label">Password</label>
-                    <input type="password" placeholder="Your Password" class="input" :class="errors && errors.password ? 'is-danger' : ''" v-model="login.password" />
-                    <p class="help is-danger" v-if="errors && errors.password">{{ errors.password[0] }}</p>
-                </div>
-                </div>
-
-                <div class="field is-marginless">
-                <div class="control">
-                    <button :disabled="isLoading" type="submit" class="button is-primary" :class="isLoading ? 'is-loading' : ''">Log in</button>
-                </div>
-                </div>
-
-            </form>
-
-          </div>
-        </div>
-      </div>
-    </section>
+        </v-dialog>
+    </v-app>
   </div>
 </template>
 
@@ -61,12 +73,43 @@
 export default {
   data() {
     return {
-      login: {
-        email: '',
-        password: ''
-      },
-      isLoading: false,
-      errors: null
+        dialog: true,
+        tab: 0,
+        tabs: [
+            {name:"Login", icon:"mdi-account"},
+            {name:"Register", icon:"mdi-account-outline"}
+        ],
+        valid: true,
+
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        verify: "",
+        login: {
+            email: '',
+            password: ''
+        },
+        errors: null,
+        loginEmailRules: [
+        v => !!v || "Required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        ],
+        emailRules: [
+        v => !!v || "Required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        ],
+
+        show1: false,
+        rules: {
+        required: value => !!value || "Required.",
+        min: v => (v && v.length >= 8) || "Min 8 characters"
+        }
+    }
+  },
+  computed: {
+    passwordMatch() {
+      return () => this.password === this.verify || "Password must match";
     }
   },
   methods: {
@@ -75,9 +118,20 @@ export default {
         let response = await this.$auth.loginWith('local', { data: this.login })
         this.$router.push('/video')
       } catch (err) {
-        this.isLoading = false
+        this.valid = false
         this.errors = err.response.data
       }
+    },
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
     }
   }
 }
